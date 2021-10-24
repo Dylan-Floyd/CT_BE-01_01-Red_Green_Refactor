599f511d8921eb95522a265f335f8048df4a3327
Dylan Floyd <floyd800@gmail.com>
2021-10-24 15:57:54 -0700

    add lab getName test

---
+++ __tests__/lab.test.js
@@ -0,0 +1,22 @@
+import { jest } from '@jest/globals';
+
+describe('lab tests', () => {
+    test('getName works', () => {
+        const data1 = {
+            name: 'spot',
+            age: 5,
+            weight: '20 lbs'
+        };
+        const expected1 = 'spot';
+        const actual1 = getName(data1);
+        expect(actual1).toEqual(expected1);
+
+        const data2 = {
+            name: 'Aang',
+            _id: '5cf56'
+        };
+        const expected2 = 'Aang';
+        const actual2 = getName(data2);
+        expect(actual2).toEqual(expected2);
+    });
+});
